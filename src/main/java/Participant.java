7001fed7a04c6a6b60c21432802c1883990c88cc
Katja <kaloh@icloud.com>
2020-01-10 18:05:18 +0100

    add class Participant

---
+++ src/main/java/Participant.java
@@ -0,0 +1,29 @@
+public class Participant {
+
+    private String name;
+    private String emailAdress;
+
+    public Participant(String name, String emailAdress) {
+        this.name = name;
+        this.emailAdress = emailAdress;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+
+}
