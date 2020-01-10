ead630e74fa127a8c6f5ec95cce1ffc488ae8e24
Katja <kaloh@icloud.com>
2020-01-10 18:05:01 +0100

    add class Pairing

---
+++ src/main/java/Pairing.java
@@ -0,0 +1,26 @@
+public class Pairing {
+
+    private Participant donee;
+    private Participant giftee;
+
+    public Pairing(Participant donee, Participant giftee) {
+        this.donee = donee;
+        this.giftee = giftee;
+    }
+
+    public Participant getDonee() {
+        return donee;
+    }
+
+    public void setDonee(Participant donee) {
+        this.donee = donee;
+    }
+
+    public Participant getGiftee() {
+        return giftee;
+    }
+
+    public void setGiftee(Participant giftee) {
+        this.giftee = giftee;
+    }
+}
