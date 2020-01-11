d90c1b309915c802b54741a6a5e3e3625dfc36f4
Katja <kaloh@icloud.com>
2020-01-11 14:30:23 +0100

    add class SecretSantaRoundLogger and add method which documents the SantaRound anonymously

---
+++ src/main/java/SecretSantaRoundLogger.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SecretSantaRoundLogger {
+
+    public void documentSecretSantaRound(ArrayList<Pairing> secretSantaRound, String year, BufferedWriter bufferedWriter) {
+
+        String pairinglog = secretSantaRound.stream()
+                .map(pairing -> pairing.getDonee().hashCode() + " : " + pairing.getGiftee().hashCode() + "\n")
+                .reduce("", this::reduceToString);
+        try {
+            bufferedWriter.write(pairinglog);
+        } catch (
+                IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private String reduceToString(String currentString, String accumulator) {
+        accumulator = accumulator + currentString;
+        return accumulator;
+    }
+}
