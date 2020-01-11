344dee42bcff5032ba7dd1dedba1d957ded9b051
Katja <kaloh@icloud.com>
2020-01-11 14:29:27 +0100

    add test method

---
+++ src/test/java/SecretSantaRoundLoggerTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class SecretSantaRoundLoggerTest {
+
+    @Test
+    public void a_round_is_documented_anonymously() throws IOException {
+        //given
+        SecretSantaRoundLogger secretSantaRoundLogger = new SecretSantaRoundLogger();
+        ArrayList<Pairing> testPairings = new ArrayList<>();
+        testPairings.add(new Pairing(
+                new Participant("Steve", "foo@bar.com"),
+                new Participant("Alice", "foo@bar.com"))
+        );
+        BufferedWriter mock = mock(BufferedWriter.class);
+
+        //when
+        secretSantaRoundLogger.documentSecretSantaRound(testPairings, "2019", mock);
+
+        //then
+        verify(mock).write(testPairings.get(0).getDonee().hashCode()+" : "+testPairings.get(0).getGiftee().hashCode()+ "\n");
+    }
+}
